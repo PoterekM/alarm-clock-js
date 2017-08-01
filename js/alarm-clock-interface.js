8e037155b0cd3824c069c6e02c5859d784f84464
Michelle Poterek and Nicole Freed <michelle@>
2017-08-01 10:30:44 -0700

    create alarm-clock.js with object declaration and isAlarmTime() prototype method

--- js/alarm-clock-interface.js
+++ js/alarm-clock-interface.js
@@ -1,11 +1,14 @@
 var AlarmClock = require('./../js/alarm-clock.js').alarmClockModule;
 
 $(document).ready(function() {
+  $('#current-time').text(moment());
   $('time-form').submit(function(event) {
     event.preventDefault();
     var time = $('#time').val();
     var newAlarmClock = new AlarmClock(time);
-    // var output = newAlarmClock.isAlarmTime(time);
-    $('#alarm').show();
+    var output = newAlarmClock.isAlarmTime(time);
+    if (output) {
+      $('#alarm').show();
+    };
   });
 });
